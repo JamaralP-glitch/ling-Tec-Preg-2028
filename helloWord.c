e9fe8a45d819af11a30938213078ed077ea3e1ab
JamaralP-glitch <joao.paulo.amaral.paula@gmail.com>
2026-07-29 21:50:18 -0300

    Update helloWord.c

--- helloWord.c
+++ helloWord.c
@@ -1,5 +1,7 @@
 # include<stdio.h>
 int main (){
-  printf( "Hello Word !\n");
+  printf( "Hello Word !\n"); // \n é um caractere da tabela representando fim de linha, que é igual a enter do teclado na linguagem C. Na tabela é 00001010 | U+000A | \n | end of line |
 return 0;
 }
+
+https://www.ime.usp.br/~pf/algorithms/appendices/ascii.html
