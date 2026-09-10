00d77270e50ad2a3a0289ae74a2eafeebfa8b8f6
JamaralP-glitch <joao.paulo.amaral.paula@gmail.com>
2026-09-09 22:44:09 -0300

    Add CPF validation functionality in Validar CPF.c
    Implement CPF validation logic and user input.

---
+++ Validar CPF.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mult(int digto, int valor){
+	return digto * valor;
+}
+
+int main() {
+
+ int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, validar;
+
+ printf("Informe seu CPF com .: ");
+ scanf("%d %d %d . %d %d %d . %d %d %d - %d %d", &n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10,&n11);
+
+printf("Conmfirme o cpf = %d%d%d.%d%d%d.%d%d%d-%d%d", n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11);
+ 
+ 
+ 	soma =  mult(n1,10)+mult(n2,9)mult(n3.8)+mult(n4,7)mult(n5,6)+mult(n6,5)mult(n7,4)+mult(n8,3)mult(n9,2);
+ 	
+ 	soma *-10;
+ 	resto - soma%11;
+ 
+ 	
+	return 0;
+}
